3be16f1d2fdcf586a382fda9f7d80e1c3e9b3eb3
codewiththapa <thapasuman2021@gmail.com>
2024-07-02 02:58:40 +0400

    day25hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect,useState,React } from "react";
 function App() {
+  const [count,setCount] = useState (0);
+  const [data,setData] = useState('suman');
+  useEffect(()=>{console.log('component mount')},[data]);
+
+  function updateCount(){
+    setCount(count+1)
+  }
+
+  function updateData(){
+    setData('Lily','thapa','ram')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{data}
+<h1>Button count {count} times.</h1>
+<button onClick={updateCount}>Click</button>
+<br /> <br />
+<button onClick={updateData}>Press</button>
+</>
   );
 }
 
